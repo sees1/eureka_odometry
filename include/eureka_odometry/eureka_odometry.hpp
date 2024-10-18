7b4cb82c2a2ba30c0f87b133898fa1dfaf113ad6
Sees <Vsevolod.Egorov@ronavi.ru>
2024-10-18 14:26:34 +0300

    new odometry by Yaroslav

--- include/eureka_odometry/eureka_odometry.hpp
+++ include/eureka_odometry/eureka_odometry.hpp
@@ -9,6 +9,8 @@
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <tf2_msgs/msg/tf_message.hpp>
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
 #include <eureka_odometry/steering_odometry.hpp>
 
 namespace eureka_odometry
@@ -29,6 +31,7 @@ namespace eureka_odometry
 
     // JointState subscriber
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
     double wheel_radius;
     double wheel_base;
@@ -36,7 +39,9 @@ namespace eureka_odometry
     double measure_error;
 
     nav_msgs::msg::Odometry odometry_msg;
+    tf2::Quaternion quaternion_;
     tf2::Quaternion orientation;
+    tf2::Quaternion rot;
 
     steering_odometry::SteeringOdometry odometry;
   };
--- src/eureka_odometry.cpp
+++ src/eureka_odometry.cpp
@@ -10,7 +10,7 @@ namespace eureka_odometry
   : rclcpp::Node("eureka_odometry"),
     wheel_radius(0.11),
     wheel_base(0.795),
-    wheel_track(0.9),
+    wheel_track(0.778),
     measure_error(0.15)
   {
     odometry_publisher = this->create_publisher<nav_msgs::msg::Odometry>(
@@ -24,35 +24,68 @@ namespace eureka_odometry
       rclcpp::SensorDataQoS(),
       std::bind(&EurekaOdometry::joint_state_subscriber_callback, this, std::placeholders::_1));
 
+    tf_broadcaster_ =std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     odometry.set_wheel_params(wheel_radius, wheel_base, wheel_track);
     odometry.init(this->now());
+
   }
 
 
   void EurekaOdometry::joint_state_subscriber_callback(const sensor_msgs::msg::JointState::SharedPtr msg)
-  {
+  {    
+    geometry_msgs::msg::TransformStamped t;
     size_t joint_count = msg->name.size();
-    double steer_angle_deg_accumulated = std::accumulate(msg->position.begin(), msg->position.end(), 0.0);
-    double steer_average_angle_deg = steer_angle_deg_accumulated / joint_count;
+    // double steer_angle_deg_accumulated = std::accumulate(msg->position.begin(), msg->position.end(), 0.0);
+    double steer_angle_deg_accumulated = msg->position[0] + msg->position[3] -msg->position[2] - msg->position[5] ; 
+    double steer_average_angle_deg = steer_angle_deg_accumulated / 4;
     double steer_average_angle_rad = steer_average_angle_deg * M_PI / 180.0;
-    double wheel_angular_velocity_accumulated = std::accumulate(msg->velocity.begin(), msg->velocity.end(), 0.0);
+    // double wheel_angular_velocity_accumulated = std::accumulate(msg->velocity.begin(), msg->velocity.end(), 0.0);
+    double wheel_angular_velocity_accumulated =  msg->velocity[0] + msg->velocity[1] + msg->velocity[2] - msg->velocity[3] - msg->velocity[4] - msg->velocity[5];
     double wheel_average_angular_velocity = wheel_angular_velocity_accumulated / joint_count;
 
     double robot_linear_velocity = wheel_average_angular_velocity * (1 - measure_error) * wheel_radius;
-    double robot_angular_velocity = std::tan(steer_average_angle_rad) * robot_linear_velocity / wheel_base;
+    double robot_angular_velocity =2*std::tan(steer_average_angle_rad) * robot_linear_velocity / wheel_base;
 
-    odometry.update_open_loop(robot_linear_velocity, robot_angular_velocity, 0.01);
+    if (msg->position[0]<-30 && msg->position[3]>30 && msg->position[2]>30 && msg->position[5]<-30)
+    {
+      steer_angle_deg_accumulated = msg->position[3] - msg->position[0] + msg->position[2] - msg->position[5];
+      steer_average_angle_deg = steer_angle_deg_accumulated / 4.0;
+      steer_average_angle_rad = steer_average_angle_deg * M_PI / 180.0; 
+      wheel_angular_velocity_accumulated = msg->velocity[0] + msg->velocity[2] + msg->velocity[3] + msg->velocity[5];
+      wheel_average_angular_velocity = -wheel_angular_velocity_accumulated / 4.0;
+      robot_linear_velocity = wheel_average_angular_velocity * (1 - measure_error) * wheel_radius;
+      robot_angular_velocity =2*std::tan(steer_average_angle_rad) * robot_linear_velocity / wheel_base;
+      robot_linear_velocity = 0.0;
+    }
+    
+    odometry.update_open_loop(robot_linear_velocity, robot_angular_velocity, 0.05);
 
     orientation.setRPY(0.0, 0.0, odometry.get_heading());
-
     odometry_msg.header.stamp = this->now();
     odometry_msg.pose.pose.position.x = odometry.get_x();
     odometry_msg.pose.pose.position.y = odometry.get_y();
     odometry_msg.pose.pose.orientation = tf2::toMsg(orientation);
     odometry_msg.twist.twist.linear.x = odometry.get_linear();
     odometry_msg.twist.twist.angular.z = odometry.get_angular();
-
     odometry_publisher->publish(odometry_msg);
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id="odom";
+    t.child_frame_id="base_link";
+    t.transform.translation.x=odometry.get_x();
+    t.transform.translation.y=odometry.get_y();
+    t.transform.translation.z=0.0;
+    t.transform.rotation=tf2::toMsg(orientation);
+    tf_broadcaster_->sendTransform(t);
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id="base_link";
+    t.child_frame_id="camera_depth_optical_frame";
+    rot.setRPY(-M_PI/2, 0.0,-M_PI/2);
+    t.transform.translation.x=0.43;
+    t.transform.translation.y=0.0;
+    t.transform.translation.z=0.0;
+    t.transform.rotation=tf2::toMsg(rot);
+    tf_broadcaster_->sendTransform(t);
+
   }
 
 } // namespace eureka_odometry
