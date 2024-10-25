046107f1837fb0524412cefef7d0a4907bd3d2c9
Sees <Vsevolod.Egorov@ronavi.ru>
2024-10-25 19:06:09 +0300

    fix: error in launch file related to quternion

--- launch/odometry.launch.py
+++ launch/odometry.launch.py
@@ -6,26 +6,39 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+  #TODO: add M_PI constant for evaluate qx and etc
   static_transform_pub_1 = Node(
     package='tf2_ros',
     executable='static_transform_publisher',
     name='base_link_to_camera',
-    arguments=['0.43', '0', '0',
-                # the order is yaw, pitch, roll
-                '-3.14159265358979323846/2', '0', '-3.14159265358979323846/2',
-                'base_link', 'camera_link'],
-    output='screen',
+    arguments=['--x', '0.43',
+               '--y', '0.0',
+               '--z', '0.0',
+               '--qx',  '-1.570796327',
+               '--qy',  '0.0',
+               '--qz',  '-1.570796327',
+               '--qw',  '1.0',
+               '--frame-id',       'base_link',
+               '--child-frame-id', 'camera_link'
+              ],
+    output='screen'
   )
 
   static_transform_pub_2 = Node(
     package='tf2_ros',
     executable='static_transform_publisher',
     name='base_link_to_camera_depth_optical',
-    arguments=['0', '0', '0',
-                # the order is yaw, pitch, roll
-                '0', '0', '0',
-                'camera_link', 'camera_depth_optical_frame'],
-    output='screen',
+    arguments=['--x', '0.0',
+              '--y',  '0.0',
+              '--z',  '0.0',
+               '--qx',  '0.0',
+               '--qy',  '0.0',
+               '--qz',  '0.0',
+               '--qw',  '1.0',
+              '--frame-id',       'camera_link',
+              '--child-frame-id', 'camera_depth_optical_frame'
+              ],
+    output='screen'
   )
 
   eureka_odometry_node = Node(
