0b3deeac57e803d11bfdcbc7110c38b1ee3438f2
Sees <Vsevolod.Egorov@ronavi.ru>
2024-11-01 16:34:23 +0300

    delete old unused static publisher

--- launch/odometry.launch.py
+++ launch/odometry.launch.py
@@ -14,32 +14,17 @@ def generate_launch_description():
     arguments=['--x', '0.43',
                '--y', '0.0',
                '--z', '0.0',
-               '--qx',  '-1.570796327',
-               '--qy',  '0.0',
-               '--qz',  '-1.570796327',
-               '--qw',  '1.0',
+               '--qx',  '-0.500',
+               '--qy',  '0.500',
+               '--qz',  '-0.500',
+               '--qw',  ' 0.500',
                '--frame-id',       'base_link',
-               '--child-frame-id', 'camera_link'
+               '--child-frame-id', 'camera_depth_optical_frame'
               ],
     output='screen'
   )
 
-  static_transform_pub_2 = Node(
-    package='tf2_ros',
-    executable='static_transform_publisher',
-    name='base_link_to_camera_depth_optical',
-    arguments=['--x', '0.0',
-              '--y',  '0.0',
-              '--z',  '0.0',
-               '--qx',  '0.0',
-               '--qy',  '0.0',
-               '--qz',  '0.0',
-               '--qw',  '1.0',
-              '--frame-id',       'camera_link',
-              '--child-frame-id', 'camera_depth_optical_frame'
-              ],
-    output='screen'
-  )
+  
 
   eureka_odometry_node = Node(
     package='eureka_odometry',
@@ -49,6 +34,5 @@ def generate_launch_description():
 
   ld = LaunchDescription()
   ld.add_action(static_transform_pub_1)
-  ld.add_action(static_transform_pub_2)
   ld.add_action(eureka_odometry_node)
   return ld
